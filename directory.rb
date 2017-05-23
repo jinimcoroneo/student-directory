fadd7f8033ba73bd08c5ee986b730a6e50a72272
jinimcoroneo <jinim1983@gmail.com>
2017-05-23 16:33:57 +0100

    creating the input students method

--- directory.rb
+++ directory.rb
@@ -1,32 +1,33 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, hit return twice"
+  students = []
+  name = gets.chomp
+  while name != "" do
+    students << {name: name, cohort: :november}
+    if students.count == 1 then puts "Now we have #{students.count} student"
+    else puts "Now we have #{students.count} students" end
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "--------------------------------"
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------------------------------------"
   puts ""
 end
 
 def print_students(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+  students.each { |student| puts "#{student[:name].capitalize} (#{student[:cohort].capitalize} cohort)" }
 end
 
 def print_footer(names)
   puts ""
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students!"
 end
 
+students = input_students
 print_header
 print_students(students)
 print_footer(students)
