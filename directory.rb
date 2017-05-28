541cf203ad9b04e51832162c860070eb2b51c685
jinimcoroneo <jinim1983@gmail.com>
2017-05-28 14:25:40 +0100

    completing exercise 7

--- directory.rb
+++ directory.rb
@@ -1,30 +1,47 @@
+@students = []
+@months = [:January, :February, :March, :April, :May, :June, :July, :August,
+            :September, :October, :November, :December]
+
 def input_students
-  puts "Please enter the name of the student"
+  puts "Please enter the details of each student"
   puts "To finish, hit return twice"
-  students = []
   print "Name: "
   name = gets.chomp
   while name != "" do
     print "Cohort: "
-    cohort = gets.chomp
+    cohort = gets.chomp.capitalize
+    if cohort == ""
+      cohort = :November
+    end
+
+    if cohort != ""
+      until @months.any? { |month| cohort.to_sym == month }
+        puts "Please enter a valid month!"
+        cohort = gets.chomp.capitalize.to_sym
+      end
+    end
+
     print "Hobbies: "
     hobbies = gets.chomp
     print "Birthplace: "
     birth_place = gets.chomp
-    students << {name: name, cohort: cohort, hobbies: hobbies, birth_place: birth_place}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort.to_sym, hobbies: hobbies, birth_place: birth_place}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     print "Name: "
     name = gets.chomp
   end
-  students
+  @students
 end
 
-def print_header(students)
-  if students.empty? == false
+
+
+
+def print_header
+  if @students.empty? == false
     puts "The students of my cohort at Makers Academy".center(100)
     puts "-------------------------------------------".center(100)
     puts ""
@@ -34,28 +51,28 @@ def print_header(students)
   end
 end
 
-def print_students(students)
-  students.each_with_index do |student, index| 
+def print_students
+  @students.each_with_index do |student, index| 
     puts "#{index + 1}. #{student[:name].capitalize} (#{student[:cohort].capitalize} cohort)".center(100)
     puts "#{student[:name].capitalize} enjoys #{student[:hobbies]} and lives in #{student[:birth_place].capitalize}".center(100)
   end
 end
 
-def print_footer(names)
+def print_footer
   puts ""
-  if names.count == 1 then puts "Overall, we have #{names.count} great student!".center(100)
-  else puts "Overall, we have #{names.count} great students!".center(100) end
+  if @students.count == 1 then puts "Overall, we have #{@students.count} great student!".center(100)
+  else puts "Overall, we have #{@students.count} great students!".center(100) end
   puts ""
 end
 
-def search_students(students)
+def search_students
   print "Would you like to search the current list of students? (y or n) "
   answer = gets.chomp.downcase
   if answer == "y"
     print "Please enter the first letter of the student's name: "
     letter = gets.chomp.downcase
     sorted_students = []
-    students.select do |student|
+    @students.select do |student|
       if student[:name].split("")[0] == letter
         sorted_students << student
       end
@@ -70,12 +87,16 @@ def search_students(students)
   end
 end
 
-def short_names(students)
+def search_by_cohort
+
+end
+
+def short_names
   print "Would you like to display the students with short names? (y or n) "
   answer = gets.chomp
   if answer == "y"
     short_names = []
-    students.select do |student|
+    @students.select do |student|
       if student[:name].split("").length <= 12
         short_names << student
       end
@@ -96,10 +117,10 @@ end
 
 
 students = input_students
-print_header(students)
-print_students(students)
+print_header
+print_students
 puts ""
-print_footer(students)
-search_students(students)
+print_footer
+search_students
 puts ""
-short_names(students)
+short_names
